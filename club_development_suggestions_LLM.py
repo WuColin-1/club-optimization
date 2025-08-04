624d0498cee6375bc565c51f873967bc65eef744
Br1d3e <earthairplane@163.com>
2025-08-04 20:03:20 +0800

    Add files via upload

--- club_development_suggestions_LLM.py
+++ club_development_suggestions_LLM.py
@@ -5,27 +5,18 @@ import pandas as pd
 from ollama import chat
 from flask import Flask,jsonify
 
-# prepare the back end interface
-# app=Flask(__name__)
-# @app.route('api/save-results',methods=['POST'])
-# def get_suggestions():
-#     output=response['message']['content']
-#     return jsonify(json.loads(output))
 
-# if __name__=="__main__":
-#     app.run(debug=True)
-# api/club_sugggestions
 
 with open('Capstone/club_population_changes.json', 'r', encoding='utf-8') as f:
     club_suggestions = json.load(f)
 club_suggestions = pd.DataFrame(club_suggestions)
 
 
-PROMPT=(f"here are the club available:{club_suggestions['club_name']}"
-        f"here are the population in this new year of each clubs{club_suggestions['population']}")
+PROMPT=(f"here are the clubs available and their populations this year:{club_suggestions}")
+
 
 response = chat(
-    model='deepseek-r1:8b', 
+    model='deepseek-coder-v2:16b', 
     think=False, 
     options={"seed": 42},
     format={
@@ -36,22 +27,26 @@ response = chat(
               "items": {
                   "type": "object",
                   "properties": {
-                      "club": {"type": "string"},
-                      "suggestion": {"type": "string"}
+                      "Club": {"type": "string"},
+                      "Activity Suggestions": {"type": "string"},
+                      "Money Distribution": {"type": "integer"},
+                      "Places": {"type": "string"}
                   },
-                  "required": ["club", "suggestion"]
+                  "required": ["Club", "Activity Suggestions", "Money Distribution", "Places"]
               }
           }
       },
       "required": ["club_suggestions"]
     }, 
+
     messages=[
         {
             "role": "system",
             "content": (
                 "You are a club assistant. Your job is to give development advice, including 场地分配、经费需求、未来活动建议 to each club "
                 "based on their population changes and also suggest funding allocations. "
-                "Output only the JSON as specified."
+                "You must include ALL 30 clubs."
+                "Offer Reasoning for each activity suggestion."
             )
         },
         {
@@ -62,6 +57,7 @@ response = chat(
 )
 
 text_response = response['message']['content']
+print(text_response)
 
 text_response = json.loads(text_response)
 
